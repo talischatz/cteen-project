e9a42de06ed9a54cb1c89e802f381a25b58403fd
Brian Gelvez <briannn97@gmail.com>
2023-11-11 15:31:41 -0300

    eliminando imagen cambiar

--- components/content/Contents.jsx
+++ components/content/Contents.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Image from "next/image";
+
 import acercadedar from '../../public/content/acercadedar.png';
 import alturismo from '../../public/content/alturismo.png';
-// import cambiar from '../../public/content/cambiar.png';
-// import cansado from '../../public/content/cansado.png';
 import costoso from '../../public/content/costoso.png';
 import creciendo from '../../public/content/creciendo.png';
 import decisiones from '../../public/content/decisiones.png';
@@ -11,7 +10,6 @@ import desafiante from '../../public/content/desafiante.png';
 import destino from '../../public/content/destino.png';
 import disciplinado from '../../public/content/disciplinado.png';
 import escuela from '../../public/content/escuela.png';
-// import grandesplanes from '../../public/content/grandesplanes.png';
 import hambrientas from '../../public/content/hambrientas.png';
 import labordeamor from '../../public/content/cambiar.png';
 import maestros from '../../public/content/maestros.png';
@@ -20,7 +18,6 @@ import nobel from '../../public/content/Nobel.png';
 import pleno from '../../public/content/pleno.png';
 import poderoso from '../../public/content/poderoso.png';
 import prefacio from '../../public/content/prefacio.png';
-// import principalobjetivo from '../../public/content/principalobjetivo.png';
 import rebe from '../../public/content/rebe.png';
 import regalo from '../../public/content/regalo.png';
 import saludable from '../../public/content/saludable.png';
