a8dc2626d74b962158157b0e6245d0fc101465d6
Brian Gelvez <briannn97@gmail.com>
2023-12-08 16:23:19 -0300

    imagenes

--- constants/NewsMockup.js
+++ constants/NewsMockup.js
@@ -1,38 +1,38 @@
 export const NewsData = [
   {
     id: 1,
-    image: require('@/public/cteen-hero.webp'),
-    title: 'Cteen Example News',
-    content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos deleniti consectetur ea! Autem distinctio, consequatur voluptas quasi vitae cum velit debitis, unde hic, repellendus ipsum perspiciatis laudantium voluptatibus necessitatibus explicabo. Repellendus, blanditiis esse animi inventore aut repellat amet fuga in minus atque, quisquam odio eos sed! Asperiores sapiente rem architecto pariatur esse ipsa, veniam odit est laboriosam ullam voluptatum fugiat nesciunt illum veritatis impedit dolores maiores nostrum. Temporibus, quasi.'
+    image: require('@/public/portada1.png'),
+    title: 'Shabbaton Cteen 2023',
+    content: 'Los chicos de 5° de liceo viajaron al shabbaton de Cteen en 2023 visitan la sede centrar de jabad "770"'
   },
   {
     id: 2,
-    image: require('@/public/cteen-hero.webp'),
-    title: 'Cteen Example News',
-    content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos deleniti consectetur ea! Autem distinctio, consequatur voluptas quasi vitae cum velit debitis, unde hic, repellendus ipsum perspiciatis laudantium voluptatibus necessitatibus explicabo. Repellendus, blanditiis esse animi inventore aut repellat amet fuga in minus atque, quisquam odio eos sed! Asperiores sapiente rem architecto pariatur esse ipsa, veniam odit est laboriosam ullam voluptatum fugiat nesciunt illum veritatis impedit dolores maiores nostrum. Temporibus, quasi.'
+    image: require('@/public/portada2.png'),
+    title: 'rockefeller center',
+    content: 'Noche de patinaje en el rockefeller center con los chicos y chicas del shabbaton de Cteen en 2023'
   },
   {
     id: 3,
-    image: require('@/public/cteen-hero.webp'),
-    title: 'Cteen Example News',
-    content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos deleniti consectetur ea! Autem distinctio, consequatur voluptas quasi vitae cum velit debitis, unde hic, repellendus ipsum perspiciatis laudantium voluptatibus necessitatibus explicabo. Repellendus, blanditiis esse animi inventore aut repellat amet fuga in minus atque, quisquam odio eos sed! Asperiores sapiente rem architecto pariatur esse ipsa, veniam odit est laboriosam ullam voluptatum fugiat nesciunt illum veritatis impedit dolores maiores nostrum. Temporibus, quasi.'
+    image: require('@/public/portada3.png'),
+    title: 'Cena de chicas',
+    content: 'Chicas cenando sushi hecho por ellas mismas y prendiendo las hanukkiot.'
   },
   {
     id: 4,
-    image: require('@/public/cteen-hero.webp'),
-    title: 'Cteen Example News',
-    content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos deleniti consectetur ea! Autem distinctio, consequatur voluptas quasi vitae cum velit debitis, unde hic, repellendus ipsum perspiciatis laudantium voluptatibus necessitatibus explicabo. Repellendus, blanditiis esse animi inventore aut repellat amet fuga in minus atque, quisquam odio eos sed! Asperiores sapiente rem architecto pariatur esse ipsa, veniam odit est laboriosam ullam voluptatum fugiat nesciunt illum veritatis impedit dolores maiores nostrum. Temporibus, quasi.'
+    image: require('@/public/portada4.png'),
+    title: 'Preparandose para el Shabatton!',
+    content: 'Chicos de 5° de liceo en el curso sinaí preparandose para el shabbaton!'
   },
   {
     id: 5,
-    image: require('@/public/cteen-hero.webp'),
-    title: 'Cteen Example News',
+    image: require('@/public/portada5.png'),
+    title: 'Tarde de Fútbol!',
     content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos deleniti consectetur ea! Autem distinctio, consequatur voluptas quasi vitae cum velit debitis, unde hic, repellendus ipsum perspiciatis laudantium voluptatibus necessitatibus explicabo. Repellendus, blanditiis esse animi inventore aut repellat amet fuga in minus atque, quisquam odio eos sed! Asperiores sapiente rem architecto pariatur esse ipsa, veniam odit est laboriosam ullam voluptatum fugiat nesciunt illum veritatis impedit dolores maiores nostrum. Temporibus, quasi.'
   },
   {
     id: 6,
-    image: require('@/public/cteen-hero.webp'),
-    title: 'Cteen Example News',
+    image: require('@/public/portada6.png'),
+    title: 'Almuerzo y shiur',
     content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos deleniti consectetur ea! Autem distinctio, consequatur voluptas quasi vitae cum velit debitis, unde hic, repellendus ipsum perspiciatis laudantium voluptatibus necessitatibus explicabo. Repellendus, blanditiis esse animi inventore aut repellat amet fuga in minus atque, quisquam odio eos sed! Asperiores sapiente rem architecto pariatur esse ipsa, veniam odit est laboriosam ullam voluptatum fugiat nesciunt illum veritatis impedit dolores maiores nostrum. Temporibus, quasi.'
   },
 ]
